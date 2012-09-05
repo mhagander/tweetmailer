c16897b3449c6b041a9d045a1e1d506a75be750f
Magnus Hagander <magnus@hagander.net>
2012-09-05 19:39:18 +0200

    Initial versoin

---
+++ tweetmailer.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+import sys
+import time
+import urllib
+import ConfigParser
+from subprocess import Popen, PIPE
+
+import simplejson as json
+
+from email.mime.text import MIMEText
+
+def sendmail(msg):
+	pipe = Popen("/usr/sbin/sendmail -t", shell=True, stdin=PIPE).stdin
+	pipe.write(msg.as_string())
+	pipe.close()
+
+if __name__=="__main__":
+	cfg = ConfigParser.ConfigParser()
+	cfg.read('tweetmailer.ini')
+
+	try:
+		twitteruser = cfg.get('twitter', 'user')
+	except:
+		print "Need option 'user' in section 'twitter' in tweetmailer.ini"
+		sys.exit(1)
+
+	try:
+		email_to = cfg.get('email', 'to')
+		email_from = cfg.get('email', 'from')
+	except:
+		print "Need options 'to' and 'from' in section 'email' in tweetmailer.ini"
+		sys.exit(1)
+
+
+	try:
+		lasttweet = int(cfg.get('twitter', 'lasttweet'))
+	except:
+		# It's ok not to have a lasttweet if this is the first run
+		lasttweet = 0
+		pass
+
+	params = {
+		"screen_name" : twitteruser,
+		"trim_user" : 1,
+		"include_entities": 0,
+		}
+	if lasttweet:
+		params['since_id'] = lasttweet
+
+	u = urllib.urlopen("http://api.twitter.com/1/statuses/user_timeline.json?%s" % urllib.urlencode(params))
+	d = json.loads(u.read())
+
+	if len(d):
+		first = True
+		newmax = lasttweet
+		for t in sorted(d, key=lambda k: k['id']):
+			if not first:
+				# Sleep if there is more than one email to send, just so we
+				# don't get rate limited or something
+				time.sleep(1)
+			else:
+				first = False
+
+			msg = MIMEText('', _charset='utf-8')
+			msg['Subject'] = t['text']
+			msg['To'] = email_to
+			msg['From'] = email_from
+			sendmail(msg)
+
+			if t['id'] > newmax:
+				newmax = t['id']
+
+		cfg.set('twitter', 'lasttweet', newmax)
+		with open('tweetmailer.ini', 'w') as f:
+			cfg.write(f)
+
